2b6185682214168a7b460bf34c1371d3ef5f2840
mbvictor <85256180+mbvictor02@users.noreply.github.com>
2023-11-30 12:45:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,24 +8,23 @@ def initialize_population(population_size):
         population[person_id] = {'infected': False, 'time_infected': 0, 'dead': False}
     return population
 
-def spread_infection(population, contagion_rate, num_interactions_per_day):
+def spread_infection(population, contagion_rate, num_interactions_per_day, mortality_rate):
     for person_id, attributes in population.items():
         if not attributes['infected'] or attributes['dead']:
             continue
         for interaction in range(num_interactions_per_day):
-            # Simulate interactions and infect others randomly
             if random.random() < contagion_rate:
                 random_person = random.choice(list(population.keys()))
-                # Don't infect already infected or dead individuals
                 if not population[random_person]['infected'] and not population[random_person]['dead']:
                     population[random_person]['infected'] = True
+                    if random.random() < mortality_rate:
+                        population[random_person]['dead'] = True
     return population
 
 def evolve_virus(day, contagion_rate, mortality_rate):
-    # Example: Contagion and mortality rates increase gradually over time
-    if day % 10 == 0:  # Increase rates every 10 days (you can adjust this)
-        contagion_rate *= 1.2  # Increase contagion rate by 20%
-        mortality_rate *= 1.1  # Increase mortality rate by 10%
+    if day % 10 == 0:
+        contagion_rate *= 1.2
+        mortality_rate *= 1.1
     return contagion_rate, mortality_rate
 
 def simulate_disease():
@@ -37,44 +36,44 @@ def simulate_disease():
     num_interactions_per_day = int(input("Enter the number of interactions per day: "))
 
     population = initialize_population(population_size)
-    infected_count_list = []  # To store infected counts for each day
-    contagion_rate_list = []  # To store contagion rates for each day
-    mortality_rate_list = []  # To store mortality rates for each day
-    
-    # Infect initial percentage of population
+    infected_count_list = []  
+    deceased_count_list = [] 
+    contagion_rate_list = [] 
+    mortality_rate_list = []  
+
     initial_infected = int(population_size * (initial_infected_percentage / 100))
     random_infected_people = random.sample(range(1, population_size + 1), initial_infected)
     for person_id in random_infected_people:
         population[person_id]['infected'] = True
 
     for day in range(1, total_duration + 1):
-        # Evolve virus over time
         contagion_rate, mortality_rate = evolve_virus(day, contagion_rate, mortality_rate)
         contagion_rate_list.append(contagion_rate)
         mortality_rate_list.append(mortality_rate)
         
-        # Simulate spread of infection
-        population = spread_infection(population, contagion_rate, num_interactions_per_day)
+        population = spread_infection(population, contagion_rate, num_interactions_per_day, mortality_rate)
         
-        # Add events like special days (e.g., Olympics)
-        if day == 50:  # Example: Olympics after 50 days
-            # Perform specific actions/events
-            pass
-        
-        # Calculate and store infected count for each day
         infected_count = sum(1 for person in population.values() if person['infected'])
         infected_count_list.append(infected_count)
-        
-    # Plotting the results
-    plt.figure(figsize=(10, 6))
+
+        deceased_count = sum(1 for person in population.values() if person['dead'])
+        deceased_count_list.append(deceased_count)
+
+    plt.figure(figsize=(10, 8))
     
-    plt.subplot(2, 1, 1)
-    plt.plot(range(1, total_duration + 1), infected_count_list, marker='o', linestyle='-')
+    plt.subplot(3, 1, 1)
+    plt.plot(range(1, total_duration + 1), infected_count_list, marker='o', linestyle='-', color='blue')
     plt.xlabel('Days')
     plt.ylabel('Infected Count')
     plt.title('Number of Infected Individuals Over Time')
     
-    plt.subplot(2, 1, 2)
+    plt.subplot(3, 1, 2)
+    plt.plot(range(1, total_duration + 1), deceased_count_list, marker='o', linestyle='-', color='red')
+    plt.xlabel('Days')
+    plt.ylabel('Deceased Count')
+    plt.title('Number of Deceased Individuals Over Time')
+
+    plt.subplot(3, 1, 3)
     plt.plot(range(1, total_duration + 1), contagion_rate_list, label='Contagion Rate', marker='o', linestyle='-')
     plt.plot(range(1, total_duration + 1), mortality_rate_list, label='Mortality Rate', marker='o', linestyle='-')
     plt.xlabel('Days')
@@ -86,4 +85,3 @@ def simulate_disease():
     plt.show()
 
 simulate_disease()
-
